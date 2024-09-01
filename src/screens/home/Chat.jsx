54c3846250b61a9e822bd9a78895374388876cc2
muku534 <prajapatimukesh0111@gmail.com>
2024-09-01 15:26:24 +0530

    Add chat archiving feature

--- src/screens/home/Chat.jsx
+++ src/screens/home/Chat.jsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, StatusBar, StyleSheet, TouchableOpacity, TextInput, Text, View, Image, FlatList } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet, TouchableOpacity, TextInput, Text, View, Image, FlatList, TouchableWithoutFeedback } from 'react-native';
 import React, { useCallback, useEffect, useState, useRef } from 'react';
 import { COLORS } from '../../../constants';
 import {
@@ -7,6 +7,7 @@ import {
 } from '../../Components/Pixel/Index';
 import fontFamily from '../../../constants/fontFamily';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Iconics from 'react-native-vector-icons/Ionicons';
 import firestore from '@react-native-firebase/firestore';
 import Entypo from 'react-native-vector-icons/Entypo';
@@ -18,9 +19,11 @@ const Chats = ({ navigation }) => {
     const [searchText, setSearchText] = useState('');
     const [filteredChats, setFilteredChats] = useState([]);
     const [updatedChats, setUpdatedChats] = useState([]);
+    const [selectedContact, setSelectedContact] = useState(null);
 
     const dispatch = useDispatch();
     const Chats = useSelector(state => state.chats);
+    const [selectedChats, setSelectedChats] = useState([]);
     const storedContacts = useSelector(state => state.contacts);
 
     useEffect(() => {
@@ -39,17 +42,27 @@ const Chats = ({ navigation }) => {
                 const contact = storedContacts.find(contact => contact.id === chat.otherUser.id);
                 if (contact) {
                     console.log("Contact found in Redux contacts:", contact);
+                    // Fetch the chat document
+                    const chatDoc = await firestore().collection('chatRooms').doc(chat.id).get();
+                    const chatData = chatDoc.data();
+                    const messages = chatData?.messages || [];
+                    const lastMessage = messages[messages.length - 1] || {};
                     return {
                         ...chat,
                         otherUser: {
                             ...contact,
                             createdAt: contact.createdAt ? contact.createdAt.toDate() : null // Adjust if needed
-                        }
+                        },
+                        message: lastMessage.text || 'No message'
                     }; // Use contact details
                 } else {
                     console.log("Contact not found, fetching from Firestore...");
                     try {
                         const userDoc = await firestore().collection('Users').doc(chat.otherUser.id).get();
+                        const chatDoc = await firestore().collection('Chats').doc(chat.id).get();
+                        const chatData = chatDoc.data();
+                        const messages = chatData?.messages || [];
+                        const lastMessage = messages[messages.length - 1] || {};
 
                         if (userDoc.exists) {
                             const userData = userDoc.data();
@@ -59,7 +72,8 @@ const Chats = ({ navigation }) => {
                                 otherUser: {
                                     ...userData,
                                     createdAt: userData.createdAt.toDate(), // Adjust based on your timestamp handling
-                                }
+                                },
+                                message: lastMessage.text || 'No message'
                             };
                         } else {
                             console.log("User document does not exist in Firestore.");
@@ -103,68 +117,140 @@ const Chats = ({ navigation }) => {
         setSearchVisible(!searchVisible);
     };
 
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <StatusBar backgroundColor={'#f2f2f2'} barStyle="dark-content" />
+    const archiveSelectedChats = async () => {
+        try {
+            await Promise.all(selectedChats.map(chat =>
+                firestore().collection('chatRooms').doc(chat.id).update({ archived: true })
+            ));
 
-            {searchVisible ? (
-                <View style={{}}>
-                    <View style={{ width: '90%', marginVertical: hp(1), marginHorizontal: wp(5), flexDirection: 'row', justifyContent: 'center', backgroundColor: '#e8e8e8', borderRadius: wp(4) }}>
-                        <TouchableOpacity onPress={toggelInput} style={{ marginLeft: wp(10) }}>
-                            <AntDesign name="arrowleft" size={hp(3)} color={COLORS.black} style={{ position: 'absolute', left: 10, top: 10 }} />
-                        </TouchableOpacity>
-                        <TextInput
-                            placeholder="Search..."
-                            placeholderTextColor={COLORS.darkgray1}
-                            keyboardType="default"
-                            autoFocus={true}
-                            value={searchText}
-                            style={{ width: '100%', marginLeft: wp(10), height: hp(6), color: COLORS.darkgray, fontFamily: fontFamily.FONTS.regular, }}
-                            onChangeText={(text) => setSearchText(text)}
-                        />
-                        {searchText.length > 0 && ( // Conditionally render the cross icon
-                            <TouchableOpacity onPress={clearSearch} style={{ position: 'absolute', right: 10, top: 10 }}>
-                                <Entypo name="cross" size={hp(3)} color={COLORS.black} />
+            // Update local state to reflect changes
+            setUpdatedChats(prevChats =>
+                prevChats.map(chat =>
+                    selectedChats.includes(chat) ? { ...chat, archived: true } : chat
+                )
+            );
+
+            // Clear selection after archiving
+            setSelectedChats([]);
+        } catch (error) {
+            console.error("Error archiving chats:", error);
+        }
+    };
+
+
+    const handleLongPress = (chat) => {
+        setSelectedChats(prevSelectedChats => {
+            if (prevSelectedChats.includes(chat)) {
+                // If chat is already selected, deselect it
+                return prevSelectedChats.filter(c => c !== chat);
+            } else {
+                // Otherwise, select the chat
+                return [...prevSelectedChats, chat];
+            }
+        });
+    };
+
+
+
+    return (
+        <TouchableWithoutFeedback onPress={() => setSelectedChats([])}>
+            <SafeAreaView style={{ flex: 1 }}>
+                <StatusBar backgroundColor={'#f2f2f2'} barStyle="dark-content" />
+
+                {searchVisible ? (
+                    <View style={{}}>
+                        <View style={{ width: '90%', marginVertical: hp(1), marginHorizontal: wp(5), flexDirection: 'row', justifyContent: 'center', backgroundColor: '#e8e8e8', borderRadius: wp(4) }}>
+                            <TouchableOpacity onPress={toggelInput} style={{ marginLeft: wp(10) }}>
+                                <AntDesign name="arrowleft" size={hp(3)} color={COLORS.black} style={{ position: 'absolute', left: 10, top: 10 }} />
+                            </TouchableOpacity>
+                            <TextInput
+                                placeholder="Search..."
+                                placeholderTextColor={COLORS.darkgray1}
+                                keyboardType="default"
+                                autoFocus={true}
+                                value={searchText}
+                                style={{ width: '100%', marginLeft: wp(10), height: hp(6), color: COLORS.darkgray, fontFamily: fontFamily.FONTS.regular, }}
+                                onChangeText={(text) => setSearchText(text)}
+                            />
+                            {searchText.length > 0 && ( // Conditionally render the cross icon
+                                <TouchableOpacity onPress={clearSearch} style={{ position: 'absolute', right: 10, top: 10 }}>
+                                    <Entypo name="cross" size={hp(3)} color={COLORS.black} />
+                                </TouchableOpacity>
+                            )}
+                        </View>
+                    </View>
+                ) : (
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', height: hp(6), padding: wp(2), marginHorizontal: wp(2.3) }}>
+                        <Text style={{ fontFamily: fontFamily.FONTS.bold, color: COLORS.darkgray, fontSize: hp(2.8), fontWeight: '700' }}>CallVerse</Text>
+                        {selectedChats.length > 0 ? (
+                            <TouchableOpacity onPress={archiveSelectedChats}>
+                                <MaterialIcons name="archive" size={hp(3.3)} color={COLORS.darkgray} />
+                            </TouchableOpacity>
+                        ) : (
+                            <TouchableOpacity onPress={toggelInput}>
+                                <Iconics name="search" size={hp(3.3)} color={COLORS.darkgray} />
                             </TouchableOpacity>
                         )}
                     </View>
-                </View>
-            ) : (
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', height: hp(6), padding: wp(2), marginHorizontal: wp(2.3) }}>
-                    <Text style={{ fontFamily: fontFamily.FONTS.bold, color: COLORS.darkgray, fontSize: hp(2.8), fontWeight: '700' }}>CallVerse</Text>
-                    <TouchableOpacity onPress={toggelInput}>
-                        <Iconics name="search" size={hp(3.3)} color={COLORS.darkgray} />
-                    </TouchableOpacity>
-                </View>
-            )}
+                )}
 
-            <View style={{ marginVertical: hp(1) }}>
-                <FlatList
-                    data={updatedChats}
-                    keyExtractor={item => item.id}
-                    renderItem={({ item }) => (
-                        <TouchableOpacity onPress={() => navigation.navigate('PersonalChats', { User: item.otherUser })} onLongPress={() => handleLongPress(item)}>
-                            <View style={{ flexDirection: 'row', padding: wp(2), alignItems: 'center', marginHorizontal: wp(2.3) }}>
-                                <Image source={{ uri: item.otherUser.photoUrl }} style={{ width: wp(13), height: wp(13), borderRadius: wp(13) }} />
-                                <View style={{ flexDirection: 'column' }}>
-                                    <Text style={{ marginLeft: wp(2.2), paddingTop: hp(0.5), fontFamily: fontFamily.FONTS.Medium, fontSize: hp(2.2), color: COLORS.darkgray }} numberOfLines={1}>{item.otherUser.name || item.otherUser.displayName}</Text>
-                                    <Text style={{ marginLeft: wp(2.2), fontFamily: fontFamily.FONTS.regular, fontSize: hp(1.8), color: COLORS.darkgray1 }} numberOfLines={1}>{item.message}</Text>
+                <View style={{ marginVertical: hp(1) }}>
+                    <FlatList
+                        data={updatedChats}
+                        keyExtractor={item => item.id}
+                        renderItem={({ item }) => (
+                            <TouchableOpacity onPress={() => selectedChats.includes(item) ? handleLongPress(item) : navigation.navigate('PersonalChats', { User: item.otherUser })} onLongPress={() => handleLongPress(item)}>
+                                <View style={{ flexDirection: 'row', padding: wp(2), alignItems: 'center', paddingHorizontal: wp(2.3), backgroundColor: selectedChats.includes(item) ? '#bcf5bc' : 'transparent', }}>
+                                    <View style={{ position: 'relative' }}>
+                                        <Image
+                                            source={{ uri: item.otherUser.photoUrl }}
+                                            style={{ width: wp(13), height: wp(13), borderRadius: wp(13) }}
+                                        />
+                                        {selectedChats.includes(item) && (
+                                            <View style={{
+                                                backgroundColor: COLORS.tertiaryWhite,
+                                                height: hp(3.5),
+                                                width: hp(3.5),
+                                                borderRadius: hp(3.5),
+                                                alignItems: 'center',
+                                                justifyContent: 'center',
+                                                position: 'absolute',
+                                                bottom: -5,
+                                                right: -5,
+                                                margin: wp(0)
+                                            }}>
+                                                <AntDesign
+                                                    name="checkcircle"
+                                                    size={hp(2.8)}
+                                                    color={COLORS.lightGreen}
+                                                />
+                                            </View>
+                                        )}
+                                    </View>
+                                    <View style={{ flexDirection: 'column', marginLeft: wp(2.2), }}>
+                                        <Text style={{ fontFamily: fontFamily.FONTS.Medium, fontSize: hp(2.2), color: COLORS.darkgray }} numberOfLines={1}>{item.otherUser.name}</Text>
+                                        <Text style={{ fontFamily: fontFamily.FONTS.regular, fontSize: hp(1.8), color: COLORS.primarygray }} numberOfLines={1}>{item.message}</Text>
+                                    </View>
                                 </View>
+                            </TouchableOpacity>
+                        )}
+                        ListEmptyComponent={() => (
+                            <View style={{ flex: 1, marginVertical: hp(30), justifyContent: 'center', alignItems: 'center' }}>
+                                <Text style={{ fontSize: hp(2.2), padding: hp(1.5), color: COLORS.secondaryGray }}>you dont have any contacts add contact click on the + button </Text>
                             </View>
-                        </TouchableOpacity>
-                    )}
-                    ListEmptyComponent={() => (
-                        <View style={{ flex: 1, marginVertical: hp(30), justifyContent: 'center', alignItems: 'center' }}>
-                            <Text style={{ fontSize: hp(2.2), padding: hp(1.5), color: COLORS.secondaryGray }}>you dont have any contacts add contact click on the + button </Text>
-                        </View>
-                    )}
-                />
-            </View>
+                        )}
+                    />
+                </View>
 
-        </SafeAreaView>
+            </SafeAreaView>
+        </TouchableWithoutFeedback>
     );
 };
 
 export default Chats;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    selectedContact: {
+        backgroundColor: '#bcf5bc', // Change the background color for the selected contact
+    },
+});
